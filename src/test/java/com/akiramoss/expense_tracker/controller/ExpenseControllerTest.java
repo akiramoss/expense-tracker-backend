757b84f87af487d8aa3549d1a84c853d346c4087
Iñaki Ramos <iramositurria@gmail.com>
2026-04-14 19:30:18 +0200

    test: add controller test for expense endpoints

---
+++ src/test/java/com/akiramoss/expense_tracker/controller/ExpenseControllerTest.java
@@ -0,0 +1,45 @@
+package com.akiramoss.expense_tracker.controller;
+
+import com.akiramoss.expense_tracker.service.ExpenseService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * 1. Test capa HTTP
+ * 2. Que los endpoints respondan
+ * 3. Integración controller -> service
+ */
+@WebMvcTest(ExpenseController.class)
+class ExpenseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ExpenseService service;
+
+    @Test
+    void shouldReturn200WhenCreatingExpense() throws Exception {
+
+        String json = """
+        {
+          "amount": 20,
+          "category": "food",
+          "description": "pizza",
+          "date": "2026-04-13"
+        }
+        """;
+
+        mockMvc.perform(post("/api/expenses")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+    }
+}
