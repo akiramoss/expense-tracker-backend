d275182cc2409167f69b82d61232d3e792438f0a
Iñaki Ramos <iramositurria@gmail.com>
2026-04-19 22:38:26 +0200

    feat: link expenses to users and groups during creation

--- src/main/java/com/akiramoss/expense_tracker/service/ExpenseService.java
+++ src/main/java/com/akiramoss/expense_tracker/service/ExpenseService.java
@@ -4,7 +4,11 @@ import com.akiramoss.expense_tracker.dto.ExpenseRequestDTO;
 import com.akiramoss.expense_tracker.dto.ExpenseResponseDTO;
 import com.akiramoss.expense_tracker.mapper.ExpenseMapper;
 import com.akiramoss.expense_tracker.model.Expense;
+import com.akiramoss.expense_tracker.model.ExpenseGroup;
+import com.akiramoss.expense_tracker.model.User;
+import com.akiramoss.expense_tracker.repository.ExpenseGroupRepository;
 import com.akiramoss.expense_tracker.repository.ExpenseRepository;
+import com.akiramoss.expense_tracker.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,12 +18,24 @@ import java.util.List;
 public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
+    private final UserRepository userRepository;
+    private final ExpenseGroupRepository groupRepository;
 
-    public ExpenseService(ExpenseRepository expenseRepository) {
+    public ExpenseService(ExpenseRepository expenseRepository,
+                          UserRepository userRepository, ExpenseGroupRepository groupRepository, ExpenseRepository repository) {
         this.expenseRepository = expenseRepository;
+        this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
     }
 
     public ExpenseResponseDTO createExpense(ExpenseRequestDTO dto) {
+
+
+        User user = userRepository.findById(dto.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        ExpenseGroup group = groupRepository.findById(dto.getGroupId())
+                .orElseThrow(() -> new RuntimeException("Group not found"));
 
         Expense expense = Expense.builder()
                 .amount(dto.getAmount())
@@ -27,11 +43,11 @@ public class ExpenseService {
                 .description(dto.getDescription())
                 .date(dto.getDate())
                 .createdAt(LocalDateTime.now())
+                .user(user)
+                .group(group)
                 .build();
 
-        Expense saved = expenseRepository.save(expense);
-
-        return ExpenseMapper.toDTO(saved);
+        return ExpenseMapper.toDTO(expenseRepository.save(expense));
     }
 
     public List<ExpenseResponseDTO> getAllExpenses() {
