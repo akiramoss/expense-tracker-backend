d25b62a585985bcf430a4aea0bcd77910e30ab69
Iñaki Ramos <iramositurria@gmail.com>
2026-04-19 22:22:21 +0200

    feat: add ExpenseGroup repository

---
+++ src/main/java/com/akiramoss/expense_tracker/repository/ExpenseGroupRepository.java
@@ -0,0 +1,8 @@
+package com.akiramoss.expense_tracker.repository;
+
+import com.akiramoss.expense_tracker.model.ExpenseGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseGroupRepository extends JpaRepository<ExpenseGroup, Long> {
+
+}
