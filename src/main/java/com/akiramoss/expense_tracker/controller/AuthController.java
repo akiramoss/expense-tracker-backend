fa6f752b57ab4b949cc0b39478ec0d36cd436f84
Iñaki Ramos <iramositurria@gmail.com>
2026-04-20 18:55:58 +0200

    refactor: standarize auth controller respnses

--- src/main/java/com/akiramoss/expense_tracker/controller/AuthController.java
+++ src/main/java/com/akiramoss/expense_tracker/controller/AuthController.java
@@ -1,15 +1,10 @@
 package com.akiramoss.expense_tracker.controller;
 
-import com.akiramoss.expense_tracker.dto.LoginRequestDTO;
-import com.akiramoss.expense_tracker.dto.RegisterRequestDTO;
-import com.akiramoss.expense_tracker.model.User;
+import com.akiramoss.expense_tracker.dto.*;
 import com.akiramoss.expense_tracker.service.AuthService;
 import com.akiramoss.expense_tracker.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,12 +15,26 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseDTO register(@RequestBody RegisterRequestDTO dto) {
-        return userService.register(dto);
+    public ApiResponse<UserResponseDTO> register(@RequestBody RegisterRequestDTO dto) {
+
+        UserResponseDTO user = userService.register(dto);
+
+        return ApiResponse.<UserResponseDTO>builder()
+                .status("success")
+                .message("User created successfully")
+                .data(user)
+                .build();
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDTO dto) {
-        return authService.login(dto);
+    public ApiResponse<String> login(@RequestBody LoginRequestDTO dto) {
+
+        String token = authService.login(dto);
+
+        return ApiResponse.<String>builder()
+                .status("success")
+                .message("Login successful")
+                .data(token)
+                .build();
     }
 }
